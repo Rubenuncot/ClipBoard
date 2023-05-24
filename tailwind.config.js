09d68d265b7a81ae25d585120662f618bc4eed9a
Practicas <ruben@gaeinformatica.com>
2023-05-24 13:28:20 +0200

    Inicialización

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    "./**/*.{html,js}",
+    "./node_modules/flowbite/**/*.js",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
